db4beefae349817b7a6c8bff2e1dee82645424a9
DarshaniKaushalya <darshanikaushalya7788dkd@gmail.com>
2022-02-12 19:24:19 +0530

    Initial commit

---
+++ src/contacts.js
@@ -0,0 +1,28 @@
+const contacts = [
+  {
+    id: 1,
+    name: "Beyonce",
+    imgURL:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQHxavZTIM0_gjLJB64In6giaxrlWGWQC6hNEmq06Ij5ZC6YGXJDvQ9VispdIrShKUT4_Q&usqp=CAU",
+    phone: "+123 456 789",
+    email: "b@beyonce.com"
+  },
+  {
+    id: 2,
+    name: "Jack Bauer",
+    imgURL:
+      "https://pbs.twimg.com/profile_images/625247595825246208/X3XLea04_400x400.jpg",
+    phone: "+987 654 321",
+    email: "jack@nowhere.com"
+  },
+  {
+    id: 3,
+    name: "Chuck Norris",
+    imgURL:
+      "https://i.pinimg.com/originals/e3/94/47/e39447de921955826b1e498ccf9a39af.png",
+    phone: "+918 372 574",
+    email: "gmail@chucknorris.com"
+  }
+];
+
+export default contacts;
